f8e8500b1c53952499774f1c081c19254e4d7b33
BaNru <admin@g63.ru>
2017-01-11 04:32:50 +0400

    Версия 1.2. Оптимизация скрипта: вынесен массив со словами-исключениями за цикл

--- forum.velomania.ru.user.js
+++ forum.velomania.ru.user.js
@@ -4,22 +4,23 @@
 // @description Удаление куплю-продам из "Что нового?"
 // @include     http://forum.velomania.ru/search.php*
 // @match       http://forum.velomania.ru/search.php*
-// @version     1.1
+// @version     1.2
 // @grant       none
 // @author      BaNru
 // @license     GNU GPL v3
 // ==/UserScript==
 
-var els = document.querySelectorAll('.prefix.understate');
+var els = document.querySelectorAll('.prefix.understate'),
+	elar = [
+		'Продам',
+		'Куплю',
+		'Продаю хардтейл',
+		'Продаю двухподвес'
+	];
+
 [].forEach.call(els, function(el) {
-  var elt = el.textContent.trim(),
-      elar = [
-            'Продам',
-            'Куплю',
-            'Продаю хардтейл',
-            'Продаю двухподвес'
-      ];
-  if(elar.indexOf(elt) >= 0){
-    el.closest('li').remove();
-  }
-})
+	var elt = el.textContent.trim();
+	if(elar.indexOf(elt) >= 0){
+    	el.closest('li').remove();
+  	}
+});
