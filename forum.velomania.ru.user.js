7dc156e70c4ca2ee0beac0765778cc805e51ce30
BaNru <admin@g63.ru>
2017-01-11 04:27:48 +0400

    Версия 1.1
    * Добавлены несколько ключевых слов на удаление
    * Сделано через массив, чтобы проще было добавлять или удалять исключения

--- forum.velomania.ru.user.js
+++ forum.velomania.ru.user.js
@@ -4,7 +4,7 @@
 // @description Удаление куплю-продам из "Что нового?"
 // @include     http://forum.velomania.ru/search.php*
 // @match       http://forum.velomania.ru/search.php*
-// @version     1
+// @version     1.1
 // @grant       none
 // @author      BaNru
 // @license     GNU GPL v3
@@ -12,8 +12,14 @@
 
 var els = document.querySelectorAll('.prefix.understate');
 [].forEach.call(els, function(el) {
-  var elt = el.textContent.trim();
-  if(elt == "Продам" || elt == "Куплю"){
+  var elt = el.textContent.trim(),
+      elar = [
+            'Продам',
+            'Куплю',
+            'Продаю хардтейл',
+            'Продаю двухподвес'
+      ];
+  if(elar.indexOf(elt) >= 0){
     el.closest('li').remove();
   }
 })
